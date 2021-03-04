36d371047da7edbe4db84d11493c4042afd1977f
Lnkyoung16 <magical3684@gmail.com>
2021-03-05 00:48:29 +0900

    homework_0304_4

--- homework _again.py
+++ homework _again.py
@@ -11,3 +11,4 @@ print(" ")
 print(divide.divide(minus.minus(100,multiple_2.multiple2(50, 13000)),300))
 
 #(3 * (16 + 13 ) - 27) / 5
+print(divide.divide(minus.minus(multiple_2.multiple2(3, add2.add(16, 13)), 27), 5))
