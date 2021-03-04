d895d39cc0ccb67280b2e00f482a701ff9466986
Lnkyoung16 <magical3684@gmail.com>
2021-03-05 00:36:16 +0900

    homework2

--- homework _again.py
+++ homework _again.py
@@ -1,4 +1,4 @@
-
+import add2
 import minus
 import divide
 import multiple_2
@@ -7,3 +7,4 @@ import multiple_2
 print(divide.divide(minus.minus(100,multiple_2.multiple2(50, 13000)),300))
 
 #(3 * (16 + 13 ) - 27) / 5
+print(divide.divide(minus.minus(multiple_2.multiple2(3, add2.add(16, 13)), 27),5))
