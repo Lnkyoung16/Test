571e24d67ec567486dcf85550113f2cc337f7d61
Lnkyoung16 <magical3684@gmail.com>
2021-03-09 20:21:10 +0900

    added gugudan script

---
+++ gugudan.py
@@ -0,0 +1,13 @@
+
+
+# for j in range(1, 10):
+#
+#     for k in range(1, 10):
+#         print(j, "*", k, "=", j * k)
+
+def gugudan(var1):
+    for i in range(1,10):
+        print(var1, "*", i, "=", var1 * i)
+
+
+gugudan(9)
